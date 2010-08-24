725ec6e051cc364f9714f0fe84e38aa97c903507
Adam Bair <adambair@gmail.com>
2010-08-24 13:02:20 -0400

    Finished lab_01

--- asteroids/game.rb
+++ asteroids/game.rb
@@ -5,6 +5,7 @@ require 'lib/gosu.for_1_8.bundle'
 class GameWindow < Gosu::Window
   def initialize
     super(640, 480, false)
+    @background_image = Gosu::Image.new(self, "assets/background.png", true)
   end
 
   # 60 times per second
@@ -14,6 +15,7 @@ class GameWindow < Gosu::Window
 
   # happens immediately after each iteration of the update method
   def draw
+    @background_image.draw(0, 0, 0)
   end
 
   def button_down(id)
--- asteroids/labs/lab_01/game.rb
+++ asteroids/labs/lab_01/game.rb
@@ -5,14 +5,17 @@ require 'lib/gosu.for_1_8.bundle'
 class GameWindow < Gosu::Window
   def initialize
     super(640, 480, false)
+    @background_image = Gosu::Image.new(self, "assets/background.png", true)
   end
 
   # 60 times per second
   def update
+    puts 'testing game loop...'
   end
 
   # happens immediately after each iteration of the update method
   def draw
+    @background_image.draw(0, 0, 0)
   end
 
   def button_down(id)
