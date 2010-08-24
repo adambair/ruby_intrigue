9657d3925a40ea2384f14cf34c841ae8161619e3
Adam Bair <adambair@gmail.com>
2010-08-24 02:36:53 -0400

    First Lab

--- asteroids/game.rb
+++ asteroids/game.rb
@@ -6,6 +6,18 @@ class GameWindow < Gosu::Window
   def initialize
     super(640, 480, false)
   end
+
+  # 60 times per second
+  def update
+  end
+
+  # happens immediately after each iteration of the update method
+  def draw
+  end
+
+  def button_down(id)
+    close if id == Gosu::KbQ
+  end
 end
 
 window = GameWindow.new
